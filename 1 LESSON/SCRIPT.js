74971e109d16cdf708bcb4e8900c5efe38664cba
andrewgarfil <120245361+andrewgarfil@users.noreply.github.com>
2022-12-26 19:51:12 +0400

    Add files via upload

---
+++ 1 LESSON/SCRIPT.js
@@ -0,0 +1,25 @@
+// let bookPric = 35
+// let bookAmount = 40
+// console.log(bookPric * bookAmount)
+
+/*  let yourdate = 1971
+  let presenttime = 2022
+  console.log((presenttime - yourdate) * 365,25)
+  if(yourdate > presenttime){
+      console.log("yourdate can't be more than present time ")
+  } 
+
+ let currentweather = 10
+ if(currentweather < 20){
+     console.log("it is cold weather")
+    
+ }
+ else{
+     console.log("it is hot weather ")
+ } */
+
+  let weatherincelsius = 24
+ let weatherinfahrenheit = (((9/5)*weatherincelsius)+32)
+  console.log(weatherinfahrenheit)
+
+
